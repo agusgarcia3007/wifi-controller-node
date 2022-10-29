9cb5e71777dcb1f4f25599afbb6f3e05f4b44f57
Mobile <agustin@getcharly.com>
2022-10-29 02:11:20 -0300

    added validations

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,25 +8,37 @@ wifi.init({
 })
 
 router.get('/networks', (req, res) => {
-  wifi.scan((error, networks) => {
-    if (!networks) res.status(500).send(error)
-    res.send(networks)
-  })
+  try {
+    wifi.scan((error, networks) => {
+      if (!networks) res.status(500).send('an error occurred 13', error)
+      res.send(networks)
+    })
+  } catch (error) {
+    res.status(500).send('an error occurred 17', error)
+  }
 })
 
 router.get('/mynetwork', (req, res) => {
-  wifi.getCurrentConnections((error, currentConnections) => {
-    if (error) res.status(500).send(error)
-    res.send(currentConnections)
-  })
+  try {
+    wifi.getCurrentConnections((error, currentConnections) => {
+      if (error) res.status(500).send('an error occurred 24', error)
+      res.send(currentConnections)
+    })
+  } catch (error) {
+    res.status(500).send('an error occurred 28', error)
+  }
 })
 
 router.post('/connect', (req, res) => {
-  const { ssid, password } = req.body
-  wifi.connect({ ssid, password }, (error) => {
-    if (error) res.status(500).send(error)
-    res.send('Connected')
-  })
+  try {
+    const { ssid, password } = req.body
+    wifi.connect({ ssid, password }, (error) => {
+      if (error) res.status(500).send(error)
+      res.send('Connected')
+    })
+  } catch (error) {
+    res.status(500).send('an error occurred 40', error)
+  }
 })
 
 router.post('/disconnect', (req, res) => {
@@ -34,11 +46,15 @@ router.post('/disconnect', (req, res) => {
   if (process.platform === 'darwin')
     res.status(500).send('Not supported on Mac OS')
 
-  const ssid = req.body
-  wifi.disconnect(ssid, (error) => {
-    if (error) res.json({ error })
-    res.send('Disconnected')
-  })
+  try {
+    const ssid = req.body
+    wifi.disconnect(ssid, (error) => {
+      if (error) res.json({ error })
+      res.send('Disconnected')
+    })
+  } catch (error) {
+    res.status(500).send('an error occurred 56', error)
+  }
 })
 
 module.exports = router
