dc66ea1e66a9c3031bd495b95417ddace2d21cb4
Mobile <agustin@getcharly.com>
2022-10-29 19:35:23 -0300

    changed controllers error handling

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,22 +8,22 @@ wifi.init({
 const getNetworks = (req, res) => {
   try {
     wifi.scan((error, networks) => {
-      if (!networks) res.status(500).send('an error occurred 13', error)
+      if (!networks) throw new Error('No networks found')
       res.send(networks)
     })
   } catch (error) {
-    res.status(500).send('an error occurred 17', error)
+    res.send('an error occurred 15', error)
   }
 }
 
 const getMyNetwork = (req, res) => {
   try {
     wifi.getCurrentConnections((error, currentConnections) => {
-      if (error) res.status(500).send('an error occurred 24', error)
+      if (error) res.send('an error occurred 22', error)
       res.send(currentConnections)
     })
   } catch (error) {
-    res.status(500).send('an error occurred 28', error)
+    res.send('an error occurred 26', error)
   }
 }
 
@@ -31,18 +31,18 @@ const connect = (req, res) => {
   try {
     const { ssid, password } = req.body
     wifi.connect({ ssid, password }, (error) => {
-      if (error) res.status(500).send(error)
+      if (error) res.send('an error occurred 34', error)
       res.send('Connected')
     })
   } catch (error) {
-    res.status(500).send('an error occurred 40', error)
+    res.send('an error occurred 38', error)
   }
 }
 
 const disconnect = (req, res) => {
   if (!req.body) res.status(400).send('Invalid Parameters')
   if (process.platform === 'darwin')
-    res.status(500).send('Not supported on Mac OS')
+    res.send('Disconnecting is not supported on macOS')
 
   try {
     const ssid = req.body
@@ -51,7 +51,8 @@ const disconnect = (req, res) => {
       res.send('Disconnected')
     })
   } catch (error) {
-    res.status(500).send('an error occurred 56', error)
+    res.send('an error occurred 54', error)
+    throw new Error(error)
   }
 }
 
